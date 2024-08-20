5ed7c44b3036ac7c06d1174b50dd2f84524bfbac
VishnuTejaPuli <VishnuTeja.Puli@ust.com>
2024-08-20 21:57:14 +0530

    first commit

---
+++ src/main/java/com/ust/boot/BootSecurityJwTApplication.java
@@ -0,0 +1,13 @@
+package com.ust.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurityJwTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSecurityJwTApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ust/boot/model/AuthenticationResponse.java
@@ -0,0 +1,31 @@
+
+package com.ust.boot.model;
+
+
+
+public class AuthenticationResponse {
+
+	private final String jwt;
+
+
+
+	public AuthenticationResponse(String jwt) {
+
+		
+
+		this.jwt = jwt;
+
+	}
+
+
+
+	public String getJwt() {
+
+		return jwt;
+
+	}
+
+
+
+}
+
